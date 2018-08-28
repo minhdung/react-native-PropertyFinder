1dbfcf228ba59df997e5b5cb09f669e1da7ae9da
minhdung <dungtm@rikkeisoft.com>
2018-08-28 14:10:16 +0700

    firt commit

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+'use strict';
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import SearchPage from './SearchPage';
+import SearchResults from './SearchResults';
+import {
+    createStackNavigator,
+} from 'react-navigation';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+const App = createStackNavigator({
+    Home: { screen: SearchPage },
+    Results: { screen: SearchResults },
+});
+export default App;
+
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
